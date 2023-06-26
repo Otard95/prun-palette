2f0fc0d60e0bee0f0bdeb53cb87e09ae6d08fe5a
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-06-26 07:31:43 +0200

    feat: Add base components. Button and text input

---
+++ src/components/text-input.ts
@@ -0,0 +1,24 @@
+/*
+    PrUn Palette - A command pallet for Prosperous Universe
+    Copyright (C) 2023  Stian Myklebostad
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+import './text-input.sass'
+import { input } from '../utils/dom'
+
+export const textInput = (value: string) => {
+  return input('text', value).att$('class', 'prun-palette prun-text-input')
+}
